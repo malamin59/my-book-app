addf41e4328c2b17e2aefe0e9c496892328b2e05
unknown <alaminhossen176466@gmail.com>
2025-04-17 09:28:07 +0600

    added card styel

--- src/pages/Book/Book.jsx
+++ src/pages/Book/Book.jsx
@@ -1,25 +1,26 @@
-import React, { use } from 'react';
+
+import { FaStarHalfAlt } from "react-icons/fa";
 
 const Book = ({singleBook}) => {
     console.log(singleBook)
-    const {bookName , author , image } = singleBook
+    const {bookName , category, rating, image } = singleBook
  
     return (
-        <div className="card bg-base-100 shadow-sm">
-        <figure className='p-3'>
-          <img className='w-50 h-55 rounded'
+        <div className="card bg-base-100 shadow-sm border p-4">
+        <figure className='p-4 bg-gray-100 w-2/3 mx-auto '>
+          <img className='h-[166px] rounded '
             src={image}
             alt="Shoes" />
         </figure>
         <div className="card-body">
           <h2 className="card-title">
-            Card Title
+            {bookName}
             <div className="badge badge-secondary">NEW</div>
           </h2>
           <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-          <div className="card-actions justify-end">
-            <div className="badge badge-outline">Fashion</div>
-            <div className="badge badge-outline">Products</div>
+          <div className="card-actions flex justify-between">
+            <div className="mr-4 text-xl">{category}</div>
+            <div className="flex items-center gap-3 font-bold text-xl">{rating} {<FaStarHalfAlt size={18} />} </div>
           </div>
         </div>
       </div>
--- src/pages/Books/Books.jsx
+++ src/pages/Books/Books.jsx
@@ -1,9 +1,8 @@
-import React, { Suspense, useEffect, useState } from 'react';
+import React, { Suspense } from 'react';
 import Book from '../Book/Book';
 
 const Books = ({data}) => {
 
-    const [allBooks, setAllBooks] = useState([]);
 
     // useEffect(()=> {
     //     fetch("bookData.json").then(res => res.json())
@@ -19,7 +18,7 @@ const Books = ({data}) => {
         <div>
             <h3 className='text-3xl text-center p-7'>hi am books </h3>
             <Suspense fallback={<span> loading.... </span>}>
-              <div className='grid lg:grid-cols-3 gap-6 '>
+              <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 px-4  mx-auto md:px-0  '>
               {
                 data.map((singleBook) => 
                 <Book singleBook={singleBook} key={singleBook.bookId}></Book> )
